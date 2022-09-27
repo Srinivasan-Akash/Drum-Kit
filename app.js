3ce38acb5c9efb9b4da0ea70854a39a7d40b688a
Srinivasan-Akash <108281031+Srinivasan-Akash@users.noreply.github.com>
2022-09-27 20:01:19 +0530

    uploaded app.js

---
+++ app.js
@@ -0,0 +1,52 @@
+let volume = document.getElementById("volume")
+let btns = document.querySelectorAll("button")
+
+let audios = []
+
+for(let i = 0; i<7; i++){
+    audios.push(new Audio(`./audio/sounds_sound-${i+1}.mp3`))
+}
+
+volume.addEventListener("change", (e) => {
+    audios.forEach((item) => {
+        item.volume = volume.value/100
+    })
+})
+
+document.querySelectorAll("button").forEach((item, index) => {
+    item.addEventListener("click", () => {
+        if(audios[index].HAVE_ENOUGH_DATA){
+            audios[index].play()
+        } else{
+            alert("PLease Wait For the audios to Load !!")
+        }
+    })
+})
+
+document.addEventListener("keydown", (event) => {
+    let keyPressed = event.key
+    console.log(keyPressed)
+    switch (keyPressed){
+        case "a":
+        case "A":
+            audios[0].play()
+        case "s":
+        case "S":
+            audios[1].play()
+        case "d":
+        case "D":
+            audios[2].play()
+        case "f":
+        case "F":
+            audios[3].play()
+        case "j":
+        case "J":
+            audios[4].play()
+        case "k":
+        case "K":
+            audios[5].play()
+        case "l":
+        case "L":
+            audios[6].play()
+    }
+})
