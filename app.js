a5fa5da64b47793d3370dc43696a473fa45df4d3
Srinivasan-Akash <108281031+Srinivasan-Akash@users.noreply.github.com>
2022-09-27 20:54:29 +0530

    improved code quality

--- app.js
+++ app.js
@@ -1,54 +1,11 @@
 let volume = document.getElementById("volume")
-let btns = document.querySelectorAll("button")
 
-let audios = [new Audio("./audio/sounds_sound-1.mp3"),
-             new Audio("./audio/sounds_sound-2.mp3"),
-             new Audio("./audio/sounds_sound-3.mp3"),
-             new Audio("./audio/sounds_sound-4.mp3"),
-             new Audio("./audio/sounds_sound-5.mp3"),
-             new Audio("./audio/sounds_sound-6.mp3"),
-             new Audio("./audio/sounds_sound-7.mp3")]
+let audios = []
 
-volume.addEventListener("change", (e) => {
-    audios.forEach((item) => {
-        item.volume = volume.value/100
-    })
-})
+for(let i = 0; i<7; i++){
+    audios.push(new Audio(`./audio/sounds_sound-${i+1}.mp3`))
+}
 
-document.querySelectorAll("button").forEach((item, index) => {
-    item.addEventListener("click", () => {
-        if(audios[index].HAVE_ENOUGH_DATA){
-            audios[index].play()
-        } else{
-            alert("PLease Wait For the audios to Load !!")
-        }
-    })
-})
-
-document.addEventListener("keydown", (event) => {
-    let keyPressed = event.key
-    console.log(keyPressed)
-    switch (keyPressed){
-        case "a":
-        case "A":
-            audios[0].play()
-        case "s":
-        case "S":
-            audios[1].play()
-        case "d":
-        case "D":
-            audios[2].play()
-        case "f":
-        case "F":
-            audios[3].play()
-        case "j":
-        case "J":
-            audios[4].play()
-        case "k":
-        case "K":
-            audios[5].play()
-        case "l":
-        case "L":
-            audios[6].play()
-    }
-})
+volume.addEventListener("change", (e) => {(adjustVolume())})
+document.querySelectorAll("button").forEach((item, index) => {checkClickEvent(item, index)})
+document.addEventListener("keydown", (event) => {checkKey(event)})
